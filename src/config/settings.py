4ced0014b3a27f89408a65221ecb8f1b3d9c9e24
Heardline <Kuznetsovjobstr@gmail.com>
2022-02-01 17:39:08 +0300

    #2 Основа + привет мир

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_clickhouse',
+    'tgbot',
+
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TELEGRAM_TOKEN = "1437768465:AAFVi0_MH84U9g3cwyC4bs6MSsfA2yXc-0k"
--- tgbot/models.py
+++ tgbot/models.py
@@ -1,8 +1,23 @@
 from uuid import uuid4
 from django.db import models
-# Create your models here.
 
-class User(models.Model):
+
+class CreateTracker(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
+
+class CreateUpdateTracker(CreateTracker):
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta(CreateTracker.Meta):
+        abstract = True
+class User(CreateUpdateTracker):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     telegram_id = models.IntegerField()
-    full_name = models.CharField(max_lenght=128)
+    full_name = models.CharField(max_length=120)
+
+    is_blocked_bot = models.BooleanField(default=False)
+
+    is_admin = models.BooleanField(default=False)
