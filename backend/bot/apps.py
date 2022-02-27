1cce94844f836929e569e970577007f2d71b8413
Heardline <Kuznetsovjobstr@gmail.com>
2022-02-27 18:01:51 +0300

    #2 Aiogram base

--- backend/bot/apps.py
+++ backend/bot/apps.py
@@ -1,20 +1,5 @@
 from django.apps import AppConfig
 
-def register(dp: Optional[Dispatcher] = None) -> None:
-    """
-    The function registers the app.
-    :param dp:
-        If Dispatcher is not None — register bots modules.
-    """
-
-    if dp is not None:
-        from .filters import register_filters
-        from bot.handlers import register_handlers
-
-        register_filters(dp)
-        register_handlers(dp)
-
-
 class BotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot'
--- backend/bot/models.py
+++ backend/bot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    message_id = models.CharField(max_length=128, null=True)
+    message_text = models.TextField(null=True)
+    from_id = models.CharField(max_length=128, null=True)
+
+    created_at = created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return f"{self.created_at} from {self.from_id}"
+    class Meta:
+        db_table = "message_telegram"
