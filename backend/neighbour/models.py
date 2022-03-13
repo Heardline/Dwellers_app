235cbdc8b336e8465710be7b56490adf8e9388f1
Heardline <Kuznetsovjobstr@gmail.com>
2022-03-13 17:13:52 +0300

    #13 #12 Подправил модель данных пользователя

--- backend/neighbour/models.py
+++ backend/neighbour/models.py
@@ -6,9 +6,11 @@ from django.db import models
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 
-    full_name = models.CharField(max_length=128, null=False)
+    full_name = models.CharField(max_length=62, null=False)
     slug = models.CharField(max_length=32, unique=True)
     address =  models.TextField(blank=True, null=True)
+    bio = models.CharField(max_length=128,null=True)
+    secret_hash = models.CharField(max_length=24, unique=True)
 
     telegram_id = models.CharField(max_length=128, null=True)
     telegram_data = models.JSONField(null=True)
@@ -17,8 +19,9 @@ class User(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     last_activity_at = models.DateTimeField(auto_now=True)
-
-    moderation_status = models.BooleanField()
+    is_activate = models.BooleanField(default=False)
+    is_moderator = models.BooleanField(default=False)
+    is_banned_until = models.DateTimeField(null=True)
 
     def __str__(self):
 	    return self.slug
@@ -30,3 +33,9 @@ class User(models.Model):
         now = datetime.utcnow()
         if self.last_activity_at < now - timedelta(minutes=5):
             return User.objects.filter(id=self.id).update(last_activity_at=now)
+
+    @property
+    def is_banned(self):
+        if self.is_moderator:
+            return False
+        return self.is_banned_until and self.is_banned_until > datetime.utcnow()
